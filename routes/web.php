1aa5ad8cd3726b4441fd720d7fdcb71ba1750b34
Santeee <aguilar.asantiago@gmail.com>
2020-06-21 06:32:43 -0300

    Pnoe q onq esto

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'CambioTurnoController@index')->name('index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
